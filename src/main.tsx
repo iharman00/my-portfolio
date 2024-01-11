3440870882097e89fb7a7671f2d4ff7bda0c38d4
Harman <harmanwork124@gmail.com>
2024-01-11 09:00:28 -0500

    Add Tailwind css and react router packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import Root from "./routes/Root.tsx";
+import Home from "./routes/Home.tsx";
+import Contact from "./routes/Contact.tsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

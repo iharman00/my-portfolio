f7a25868b08e2809ece636274dea8d676909838c
Harmanpreet Singh <131783243+iharman00@users.noreply.github.com>
2024-01-11 14:10:48 +0000

    Add Header and Footer compononents

---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div className="w-full h-16 bg-blue-500">Footer</div>;
+};
+
+export default Header;
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom";
 
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
+
 const Root = () => {
   return (
     <>
+      <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };

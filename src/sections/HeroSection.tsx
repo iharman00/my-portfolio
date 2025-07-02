be25a7150c9c5a7611044f5b4735b81882051cf4
Harman <165360852+Harmanpreet69@users.noreply.github.com>
2025-07-02 19:57:50 -0400

    Added image in hero section

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { getSection } from "../client.ts";
+import { getSection, urlFor } from "../client.ts";
 import { PortableText } from "@portabletext/react";
 import Reveal from "../animations/Reveal.tsx";
 import { Link } from "../components/Links.tsx";
@@ -24,14 +24,27 @@ const HeroSection = () => {
     makeGetSectionReq();
   }, []);
 
+  console.log(section);
+
   return (
-    <section id="home" className="container pt-32 py-20 xl:py-40">
+    <section id="home" className="container pt-32 lg:py-20 xl:py-20">
       <div className="wrapper">
         <Reveal>
-          <div className="lg:max-w-[70%] 2xl:max-w-[50%]">
-            <PortableText value={section?.title} />
-            <PortableText value={section?.content} />
-            <Link href="#contact">Let's talk!</Link>
+          <div className="flex flex-col lg:flex-row justify-between items-center gap-20 md:gap-32">
+            <div className="lg:max-w-[70%] 2xl:max-w-[50%]">
+              <PortableText value={section?.title} />
+              <PortableText value={section?.content} />
+              <Link href="#contact">Let's talk!</Link>
+            </div>
+            {section?.img && section?.imgAlt && (
+              <div className="relative w-full md:max-w-[60%] lg:max-w-[30%] aspect-[3/4] rounded-lg shadow-lg overflow-hidden mx-auto lg:mx-0">
+                <img
+                  src={urlFor(section?.img).url()}
+                  alt={section?.imgAlt}
+                  className="w-full h-full object-cover rounded-lg transition-transform duration-300 ease-in-out hover:scale-105"
+                />
+              </div>
+            )}
           </div>
         </Reveal>
       </div>
